0397867c75b33553b21146db02f2e7d06c1b6d63
Ben Russell <thematrixeatsyou@gmail.com>
2016-11-08 20:37:57 +1300

    EXPERIMENTAL netplay now spectatable to some extent

--- bots/net.c
+++ bots/net.c
@@ -19,31 +19,35 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define BACKLOG_CAP 4096
+#define PLAYER_MAX 2
+
 static int sockfd;
 #ifdef SERVER
-#define PLAYER_MAX 2
 #define CLIENT_MAX 256
 static uint32_t cli_player[CLIENT_MAX];
 static struct sockaddr *cli_addr[CLIENT_MAX];
 static socklen_t cli_addrlen[CLIENT_MAX];
 static int32_t player_cli[PLAYER_MAX] = {-1, -1};
-static uint32_t player_frame_idx[PLAYER_MAX];
 static uint32_t player_initial_frame_idx[PLAYER_MAX];
+static uint32_t player_frame_idx[PLAYER_MAX];
 #else
 static struct sockaddr *serv_addr;
 static socklen_t serv_addrlen;
+uint32_t serv_frame_idx;
+uint32_t serv_frame_arrived[BACKLOG_CAP];
 static uint32_t initial_backlog = 0;
 #endif
 
 // a minute should be long enough
-#define BACKLOG_CAP 4096
 static struct SMS backlog[BACKLOG_CAP];
 static uint8_t input_log[BACKLOG_CAP][2];
+uint16_t input_pmask[BACKLOG_CAP];
 static uint32_t backlog_idx = 0;
 
 static uint8_t net_intro_packet[13];
 
-const uint8_t player_masks[2][2] = {
+const uint8_t player_masks[PLAYER_MAX][2] = {
 	{0x3F, 0x00},
 	{0xC0, 0x0F},
 };
@@ -65,6 +69,39 @@ static uint32_t crc32_sms_net(uint8_t *data, size_t len, uint32_t crc)
 
 	return ~crc;
 }
+
+#ifdef SERVER
+static void detach_client(int cidx)
+{
+	// Don't try to detach unconnected clients
+	if(cidx < 0 || cidx >= CLIENT_MAX) {
+		return;
+	}
+
+	printf("Detaching client %5d\n", cidx);
+
+	int pidx = cli_player[cidx];
+	if(pidx != -1) {
+		player_cli[pidx] = -1;
+	}
+	cli_addrlen[cidx] = 0;
+	free(cli_addr[cidx]);
+	cli_addr[cidx] = NULL;
+
+}
+
+static void kick_client(const char *reason, int cidx, void *maddr, socklen_t maddr_len)
+{
+	assert(reason[0] == '\x02');
+
+	// Send message
+	sendto(sockfd, reason, strlen(reason)+1, 0,
+		(struct sockaddr *)&maddr, maddr_len);
+
+	// Check if client needs detachment
+	detach_client(cidx);
+}
+#endif
 
 uint8_t bot_hook_input(struct SMS *sms, uint64_t timestamp, int port)
 {
@@ -148,6 +185,16 @@ uint8_t bot_hook_input(struct SMS *sms, uint64_t timestamp, int port)
 
 void bot_update()
 {
+	// Save backlog frame (for sync purposes)
+	sms_copy(&backlog[backlog_idx & (BACKLOG_CAP-1)], &sms_current);
+
+#ifndef SERVER
+	// Save frame input
+	input_log[backlog_idx & (BACKLOG_CAP-1)][0] = sms_current.joy[0];
+	input_log[backlog_idx & (BACKLOG_CAP-1)][1] = sms_current.joy[1];
+	input_pmask[backlog_idx & (BACKLOG_CAP-1)] |= player_control;
+#endif
+
 #ifdef SERVER
 	// Do a quick check
 	if(player_cli[0] < 0) {
@@ -156,7 +203,9 @@ void bot_update()
 	if(player_cli[1] < 0) {
 		player_frame_idx[1] = backlog_idx;
 	}
+
 	// Get messages
+	printf("=== SFrame %d\n", backlog_idx);
 	for(;;)
 	{
 		bool jump_out = false;
@@ -165,7 +214,9 @@ void bot_update()
 		// Unless we have no players, in which case wait until we have players
 		int32_t wldiff0 = (int32_t)(backlog_idx-player_frame_idx[0]);
 		int32_t wldiff1 = (int32_t)(backlog_idx-player_frame_idx[1]);
+		//printf("wldiffs %d %d\n", wldiff0, wldiff1);
 		if((wldiff0 < 0 || wldiff1 < 0) && (player_cli[0] >= 0 || player_cli[1] >= 0)) {
+			//printf("jump-out activated\n");
 			jump_out = true;
 		}
 
@@ -175,7 +226,9 @@ void bot_update()
 			socklen_t maddr_len = sizeof(maddr);
 			memset(mbuf, 0, sizeof(mbuf));
 			ssize_t rlen = recvfrom(sockfd, mbuf, sizeof(mbuf),
-				(jump_out ? MSG_DONTWAIT : 0), (struct sockaddr *)&maddr, &maddr_len);
+				//(jump_out ? MSG_DONTWAIT : 0),
+				MSG_DONTWAIT,
+				(struct sockaddr *)&maddr, &maddr_len);
 
 			if(rlen < 0) {
 				break;
@@ -193,7 +246,7 @@ void bot_update()
 				}
 			}
 
-			printf("SMSG %5d %02X %d\n", (int)rlen, mbuf[0], cidx);
+			//printf("SMSG %5d %02X %d\n", (int)rlen, mbuf[0], cidx);
 
 			if(cidx == -1 && mbuf[0] == '\x01') {
 				// Someone wants to connect!
@@ -201,17 +254,11 @@ void bot_update()
 
 				// Validate data
 				if(rlen != sizeof(net_intro_packet)) {
-					sendto(sockfd,
-						"\x02""Bad intro\x00", 11, 0,
-						(struct sockaddr *)&maddr,
-						maddr_len);
+					kick_client("\x02""Bad intro", cidx, maddr, maddr_len);
 					continue;
 
 				} else if(0 != memcmp(net_intro_packet, mbuf, rlen)) {
-					sendto(sockfd,
-						"\x02""Bad emu state\x00", 15, 0,
-						(struct sockaddr *)&maddr,
-						maddr_len);
+					kick_client("\x02""Bad emu state", cidx, maddr, maddr_len);
 					continue;
 
 				}
@@ -224,11 +271,8 @@ void bot_update()
 					}
 				}
 
-				if(cidx == -1) {
-					sendto(sockfd,
-						"\x02""Server full\x00", 13, 0,
-						(struct sockaddr *)&maddr,
-						maddr_len);
+				if(cidx < 0 || cidx >= CLIENT_MAX) {
+					kick_client("\x02Server full", cidx, maddr, maddr_len);
 					continue;
 				}
 
@@ -237,10 +281,12 @@ void bot_update()
 				if(player_cli[0] < 0) {
 					pidx = 0;
 				} else if(player_cli[1] < 0) {
-					pidx = 1;
+					// TODO: get nonspecs to sync
+					//pidx = 1;
 				}
 
 				cli_player[cidx] = pidx;
+				printf("client %5d -> player %5d\n", cidx, pidx);
 				if(pidx != -1) {
 					player_cli[pidx] = cidx;
 					player_frame_idx[pidx] = backlog_idx;
@@ -262,10 +308,7 @@ void bot_update()
 
 			} else if(cidx == -1) {
 				// We don't have this client, kick it
-				sendto(sockfd,
-					"\x02""Not connected\x00", 15, 0,
-					(struct sockaddr *)&maddr,
-					maddr_len);
+				kick_client("\x02Not connected", cidx, maddr, maddr_len);
 
 			} else if(mbuf[0] == '\x01') {
 				// Did they not get the intro? Resend the intro.
@@ -284,19 +327,132 @@ void bot_update()
 				// Someone's sick of us!
 				mbuf[sizeof(mbuf)-1] = '\x00';
 				printf("Client disconnected: \"%s\"\n", mbuf+1);
+				detach_client(cidx);
 
-				int pidx = cli_player[cidx];
-				if(pidx != -1) {
-					player_cli[pidx] = -1;
+			} else if(mbuf[0] == '\x03') {
+				// Do they need a given state? Send it!
+				uint32_t frame_idx = *((uint32_t *)(mbuf+1));
+				uint32_t offs = *((uint32_t *)(mbuf+5));
+				uint32_t len = *((uint16_t *)(mbuf+9));
+				if(offs > sizeof(struct SMS) || offs+len > sizeof(struct SMS)) {
+					kick_client("\x02""Out of bounds state read", cidx, maddr, maddr_len);
+					continue;
 				}
-				cli_addrlen[cidx] = 0;
-				free(cli_addr[cidx]);
-				cli_addr[cidx] = NULL;
+				if(len > 1024 || len < 1) {
+					kick_client("\x02""Sync length bad", cidx, maddr, maddr_len);
+					continue;
+				}
+
+				mbuf[0] = 0x04;
+				uint8_t *ps = mbuf+11;
+				uint8_t *pd = (uint8_t *)&backlog[frame_idx&(BACKLOG_CAP-1)];
+				memcpy(ps, pd+offs, len);
+				sendto(sockfd, mbuf, 11+len, 0,
+					(struct sockaddr *)&maddr, maddr_len);
+
+			} else if(mbuf[0] == '\x05') {
+				// Did they not get a set of inputs? Resend it!
+				uint32_t in_beg = ((uint32_t *)(mbuf+1))[0];
+
+				// Of course, we have to have the inputs available...
+				if((backlog_idx-in_beg) >= BACKLOG_CAP-1) {
+					kick_client("\x02""End of backlog reached", cidx, maddr, maddr_len);
+					continue;
+				}
+
+				// Get length and clamp max at now / clamp max at max
+				uint32_t in_len = mbuf[5];
+				if((int32_t)(backlog_idx-(in_beg+in_len)) < 0) {
+					in_len = backlog_idx-in_beg;
+				}
+
+				// Skip if length == 0
+				if(in_len == 0) {
+					continue;
+				}
+
+				// Now produce backlog!
+				mbuf[0] = 0x06;
+				((uint32_t *)(mbuf+1))[0] = in_beg;
+				assert(in_len >= 1 && in_len <= 255);
+				mbuf[5] = in_len;
+				uint8_t *p = &mbuf[6];
+				for(uint32_t i = 0; i < in_len; i++) {
+					p[i*2+0] = input_log[(in_beg+i)&(BACKLOG_CAP-1)][0];
+					p[i*2+1] = input_log[(in_beg+i)&(BACKLOG_CAP-1)][1];
+				}
+
+				// Send it!
+				sendto(sockfd, mbuf, 6+2*in_len, 0,
+					(struct sockaddr *)&maddr, maddr_len);
+
+			} else if(mbuf[0] == '\x06') {
+				int pidx = cli_player[cidx];
+				if(pidx < 0 || pidx >= PLAYER_MAX) {
+					// Extraneous packet - spectators do NOT send input
+					continue;
+				}
+
+				// WE HAVE INPUTS
+				uint32_t frame_idx = *((uint32_t *)(mbuf+1));
+				uint32_t len = mbuf[5];
+
+				// Ensure we are in range
+				int32_t fdelta0 = (int32_t)(frame_idx-backlog_idx);
+				int32_t fdelta1 = fdelta0+len;
+				if(fdelta0 >= BACKLOG_CAP/4 || -fdelta0 >= BACKLOG_CAP-1) {
+					kick_client("\x02""Frame out of range", cidx, maddr, maddr_len);
+					continue;
+				}
+				if(fdelta1 >= BACKLOG_CAP/4 || -fdelta1 >= BACKLOG_CAP-1) {
+					kick_client("\x02""Frame out of range", cidx, maddr, maddr_len);
+					continue;
+				}
+
+				// Advance if necessary
+				int32_t pfdelta = (int32_t)(frame_idx+len-player_frame_idx[pidx]);
+				if(pfdelta > 0) {
+					player_frame_idx[pidx] = frame_idx+len;
+					//printf("Advance %d\n", frame_idx+len);
+				}
+
+				// Set inputs
+				mbuf[0] = 0x06;
+				uint8_t *p = mbuf+6;
+				for(int i = 0; i < len; i++) {
+					int idx = (frame_idx+i)&(BACKLOG_CAP-1);
+					uint8_t old0 = input_log[idx][0];
+					uint8_t old1 = input_log[idx][1];
+					uint8_t m0 = player_masks[pidx][0];
+					uint8_t m1 = player_masks[pidx][1];
+					uint8_t new0 = p[2*i+0];
+					uint8_t new1 = p[2*i+1];
+					new0 = old0^((old0^new0)&m0);
+					new1 = old1^((old1^new1)&m1);
+					//p[2*i+0] = input_log[idx][0] = backlog[idx].joy[0] = new0;
+					//p[2*i+1] = input_log[idx][1] = backlog[idx].joy[1] = new1;
+					p[2*i+0] = input_log[idx][0] = new0;
+					p[2*i+1] = input_log[idx][1] = new1;
+					input_pmask[idx] |= (1<<pidx);
+				}
+				//printf("%3d INPUT\n", pidx);
+
+				// Send it to the other clients!
+				for(int ci = 0; ci < CLIENT_MAX; ci++) {
+					//if(cli_addrlen[ci] != 0 && ci != cidx) {
+					if(cli_addrlen[ci] != 0 && ci != cidx) {
+						sendto(sockfd, mbuf, 6+2*len, 0,
+							cli_addr[ci], cli_addrlen[ci]);
+					}
+				}
+
 			}
 		}
 
 		if(jump_out) {
 			break;
+		} else {
+			usleep(10000);
 		}
 	}
 #else
@@ -312,18 +468,31 @@ void bot_update()
 			MSG_DONTWAIT, (struct sockaddr *)&maddr, &maddr_len);
 
 		if(rlen < 0) {
+			if(player_id < 0) {
+				// Wait for messages
+				if((int32_t)(backlog_idx-serv_frame_idx) >= 0) {
+					usleep(1000);
+					continue;
+				}
+			}
+
+			// Check if we need to keep going
+			if(false) {
+				usleep(1000);
+				continue;
+			}
 			break;
 		}
 
-		printf("CMSG %d %02X\n", (int)rlen, mbuf[0]);
+		//printf("CMSG %d %02X\n", (int)rlen, mbuf[0]);
 
 		if(mbuf[0] == '\x01') {
 			// We connected! Again!
 			printf("Acknowledged! Again!\n");
 			initial_backlog = ((uint32_t *)(mbuf+1))[0];
-			player_id = ((int32_t *)(mbuf+1))[0];
+			player_id = ((int32_t *)(mbuf+1))[1];
 			backlog_idx = initial_backlog;
-			player_control = (player_id >= 0 && player_id <= 31
+			player_control = ((player_id >= 0 && player_id <= 31)
 				? (1<<player_id) : 0);
 
 		} else if(mbuf[0] == '\x02') {
@@ -332,24 +501,145 @@ void bot_update()
 			printf("KICKED: \"%s\"\n", mbuf+1);
 			fflush(stdout);
 			abort();
+
+		} else if(mbuf[0] == '\x04') {
+			// New state!
+			// XXX: do we really want to bother with this?
+			// It can be sent extraneously in theory,
+			// but that's all really
+			//printf("STATE\n");
+
+		} else if(mbuf[0] == '\x05') {
+			// Server missed our inputs - resend them!
+			uint32_t in_beg = ((uint32_t *)(mbuf+1))[0];
+
+			// Of course, we have to have the inputs available...
+			assert(!((backlog_idx-in_beg) >= BACKLOG_CAP-1));
+
+			// Get length and clamp max at now / clamp max at max
+			uint32_t in_len = mbuf[5];
+			if((int32_t)(backlog_idx-(in_beg+in_len)) < 0) {
+				in_len = backlog_idx-in_beg;
+			}
+
+			// Skip if length == 0
+			if(in_len == 0) {
+				continue;
+			}
+
+			// Now produce backlog!
+			mbuf[0] = 0x06;
+			((uint32_t *)(mbuf+1))[0] = in_beg;
+			assert(in_len >= 1 && in_len <= 255);
+			mbuf[5] = in_len;
+			uint8_t *p = &mbuf[6];
+			for(uint32_t i = 0; i < in_len; i++) {
+				p[i*2+0] = input_log[(in_beg+i)&(BACKLOG_CAP-1)][0];
+				p[i*2+1] = input_log[(in_beg+i)&(BACKLOG_CAP-1)][1];
+			}
+
+			// Send it!
+			sendto(sockfd, mbuf, 6+2*in_len, 0,
+				(struct sockaddr *)&maddr, maddr_len);
+
+		} else if(mbuf[0] == '\x06') {
+			// New inputs!
+			uint32_t in_beg = ((uint32_t *)(mbuf+1))[0];
+			uint32_t in_len = mbuf[5];
+			uint8_t *p = &mbuf[6];
+			printf("INPUT %10d %3d\n", in_beg, in_len);
+
+			uint8_t m0 = (player_id >= 0 ? player_masks[player_id][0] : 0x00);
+			uint8_t m1 = (player_id >= 0 ? player_masks[player_id][1] : 0x00);
+			for(int i = 0; i < in_len; i++) {
+				int idx = (in_beg+i) & (BACKLOG_CAP-1);
+				/*
+				input_log[idx][0] &= m0;
+				input_log[idx][1] &= m1;
+				input_log[idx][0] |= p[i*2+0] & ~m0;
+				input_log[idx][1] |= p[i*2+1] & ~m1;
+				*/
+				input_log[idx][0] = p[i*2+0];
+				input_log[idx][1] = p[i*2+1];
+				backlog[idx].joy[0] = input_log[idx][0];
+				backlog[idx].joy[1] = input_log[idx][1];
+			}
+
+			//assert(!((backlog_idx-in_beg) >= BACKLOG_CAP-1));
+
+			// FIXME do this properly to avoid desyncs
+			int idx = (backlog_idx) & (BACKLOG_CAP-1);
+			input_log[idx][0] &= m0;
+			input_log[idx][1] &= m1;
+			input_log[idx][0] |= p[(in_len-1)*2+0] & ~m0;
+			input_log[idx][1] |= p[(in_len-1)*2+1] & ~m1;
+			backlog[idx].joy[0] = input_log[idx][0];
+			backlog[idx].joy[1] = input_log[idx][1];
+			printf("INPUT %02X %02X\n"
+				, backlog[idx].joy[0]
+				, backlog[idx].joy[1]
+				);
+
+			// Also, server is now ahead
+			serv_frame_idx = in_beg+in_len;
 		}
 	}
 #endif
 
-	// Save frame
 	uint32_t idx = (backlog_idx&(BACKLOG_CAP-1));
-	sms_copy(&backlog[idx], &sms_current);
-	input_log[idx][0] = sms_current.joy[0];
-	input_log[idx][1] = sms_current.joy[1];
+
+	// Restore frame input
+	sms_current.joy[0] = input_log[idx][0];
+	sms_current.joy[1] = input_log[idx][1];
+
+#ifndef SERVER
+	if(player_id >= 0) {
+		// Send frame input to server
+		// TODO: batch these!
+		uint8_t myinputpkt[8];
+		myinputpkt[0] = 0x06;
+		((uint32_t *)(myinputpkt+1))[0] = idx;
+		myinputpkt[5] = 0x01;
+		myinputpkt[6] = input_log[idx][0];
+		myinputpkt[7] = input_log[idx][1];
+		sendto(sockfd, myinputpkt, sizeof(myinputpkt), 0,
+			serv_addr, serv_addrlen);
+	}
+#endif
+
+	// Load frame backup
+	backlog[idx].joy[0] = input_log[idx][0];
+	backlog[idx].joy[1] = input_log[idx][1];
+	sms_copy(&sms_current, &backlog[idx]);
 	backlog_idx++;
 
-	//twait = time_now()-20000;
+#ifndef SERVER
+
+	// If spectator, play catchup if too far behind
+	if(player_id < 0) {
+		printf("***** SPEC %d\n", backlog_idx);
+		if((int32_t)(serv_frame_idx-backlog_idx) >= 20) {
+			twait = time_now()-FRAME_WAIT*15;
+		}
+	}
+
+	// Disable no_draw
+	sms_current.no_draw = false;
+#endif
+
+#ifdef SERVER
+
+	// If server... don't wait.
+	twait = time_now()-FRAME_WAIT;
+#endif
 }
 
 void bot_init(int argc, char *argv[])
 {
 	sms_current.joy[0] = 0xFF;
 	sms_current.joy[1] = 0xFF;
+
+	memset(input_log, 0xFF, sizeof(input_log));
 
 	sms_rom_crc = crc32_sms_net(sms_rom, sizeof(sms_rom), 0);
 	//sms_rom_crc = crc32_sms_net(sms_rom, 512*1024, 0);
@@ -491,19 +781,21 @@ void bot_init(int argc, char *argv[])
 				break;
 			}
 
-			printf("CMSG %d %02X\n", (int)rlen, mbuf[0]);
+			//printf("CMSG %d %02X\n", (int)rlen, mbuf[0]);
 
 			if(mbuf[0] == '\x01') {
 				// We connected!
 				assert(rlen == 9);
-				printf("Acknowledged!\n");
 				initial_backlog = ((uint32_t *)(mbuf+1))[0];
-				player_id = ((int32_t *)(mbuf+1))[0];
+				player_id = ((int32_t *)(mbuf+1))[1];
 				backlog_idx = initial_backlog;
-				player_control = (player_id >= 0 && player_id <= 31
+				player_control = ((player_id >= 0 && player_id <= 31)
 					? (1<<player_id) : 0);
+				printf("Acknowledged! id=%02d control=%08X\n"
+					, player_id, player_control);
 				connected = true;
 				break;
+
 			} else if(mbuf[0] == '\x02') {
 				// The server hates us!
 				mbuf[sizeof(mbuf)-1] = '\x00';
@@ -516,7 +808,112 @@ void bot_init(int argc, char *argv[])
 	assert(connected);
 
 	// Sync state
-	// TODO!
+	uint8_t has_byte[sizeof(struct SMS)];
+	uint32_t bytes_remain = sizeof(struct SMS);
+	memset(has_byte, 0x00, sizeof(has_byte));
+
+	printf("Sending sync requests...\n");
+	for(uint32_t i = 0; i < sizeof(struct SMS); ) {
+		uint32_t nlen = sizeof(struct SMS)-i;
+		if(nlen > 1024) {
+			nlen = 1024;
+		}
+
+		uint8_t pktbuf[11];
+		pktbuf[0] = 0x03;
+		*((uint32_t *)(pktbuf+1)) = backlog_idx;
+		*((uint32_t *)(pktbuf+5)) = i;
+		*((uint16_t *)(pktbuf+9)) = nlen;
+		sendto(sockfd, pktbuf, sizeof(pktbuf), 0, serv_addr, serv_addrlen);
+
+		i += nlen;
+	}
+
+	printf("Syncing state...\n");
+	int sleeps_until_resync = 10;
+	while(bytes_remain > 0) {
+		usleep(100000);
+		bool had_packet = false;
+		for(;;) {
+			uint8_t mbuf[2048];
+			uint8_t maddr[128];
+			socklen_t maddr_len = sizeof(maddr);
+			memset(mbuf, 0, sizeof(mbuf));
+			ssize_t rlen = recvfrom(sockfd, mbuf, sizeof(mbuf),
+				MSG_DONTWAIT, (struct sockaddr *)&maddr, &maddr_len);
+
+			if(rlen < 0) {
+				break;
+			}
+
+			//printf("CMSG %d %02X\n", (int)rlen, mbuf[0]);
+
+			if(mbuf[0] == '\x04') {
+				had_packet = true;
+				// Synced state has arrived!
+				assert(rlen >= 11);
+				uint32_t frame_idx = *((uint32_t *)(mbuf+1));
+				uint32_t offs = *((uint32_t *)(mbuf+5));
+				uint32_t len = *((uint16_t *)(mbuf+9));
+				printf("Sync packet: frame_idx=%08X, offs=%08X, len=%04X\n"
+					, frame_idx, offs, len);
+				uint8_t *ps = mbuf+11;
+				uint8_t *pd = (uint8_t *)&sms_current;
+				assert(offs <= sizeof(struct SMS));
+				assert(offs+len <= sizeof(struct SMS));
+				memcpy(pd+offs, ps, len);
+				for(int i = 0; i < len; i++) {
+					if(has_byte[offs+i] == 0){
+						has_byte[offs+i] = 0xFF;
+						bytes_remain--;
+					}
+				}
+
+			} else if(mbuf[0] == '\x02') {
+				// The server hates us!
+				mbuf[sizeof(mbuf)-1] = '\x00';
+				printf("KICKED: \"%s\"\n", mbuf+1);
+				fflush(stdout);
+				abort();
+			}
+		}
+
+		if(!had_packet) {
+			sleeps_until_resync--;
+			if(sleeps_until_resync <= 0) {
+				printf("Sending resync (%d remain)\n", bytes_remain);
+				sleeps_until_resync = 10;
+
+				for(uint32_t i = 0; i < sizeof(struct SMS); ) {
+					uint32_t nlen = sizeof(struct SMS)-i;
+					if(nlen > 1024) {
+						nlen = 1024;
+					}
+
+					bool need_byte = false;
+					for(int j = 0; j < nlen; j++) {
+						if(has_byte[i+j] == 0) {
+							need_byte = true;
+							break;
+						}
+					}
+
+					if(need_byte) {
+						uint8_t pktbuf[11];
+						pktbuf[0] = 0x03;
+						*((uint32_t *)(pktbuf+1)) = backlog_idx;
+						*((uint32_t *)(pktbuf+5)) = i;
+						*((uint16_t *)(pktbuf+9)) = nlen;
+						sendto(sockfd, pktbuf, sizeof(pktbuf), 0, serv_addr, serv_addrlen);
+					}
+
+					i += nlen;
+				}
+			}
+		}
+	}
+
+	printf("State is now synced. Let's go.\n");
 #endif
 	printf("ROM CRC: %04X\n", sms_rom_crc);
 
